d67e5ea29b9de0be10493c3f4c329f5ffa52b6ba
Esger <esgerj@gmail.com>
2024-02-04 22:11:40 +0100

    bal gaat met eenparige beweging naar klik

--- src/resources/elements/ball/ball.js
+++ src/resources/elements/ball/ball.js
@@ -1,9 +1,41 @@
-import {bindable} from 'aurelia-framework';
+import { inject } from 'aurelia-framework';
 
+@inject(Element)
 export class Ball {
-  @bindable value;
+    speed = 500; // px/s
+    constructor(element) {
+        this._element = element;
+        $('body').on('click', event => {
+            this.moveTo(event.clientX, event.clientY);
+        });
+    }
 
-  valueChanged(newValue, oldValue) {
-    //
-  }
+    attached() {
+    }
+
+    _setCurrentPosition() {
+        this.currentPosition = {
+            x: this._element.getBoundingClientRect().x,
+            y: this._element.getBoundingClientRect().y,
+        }
+    }
+
+    _setTransitionTime(x, y) {
+        const dx = x - this.currentPosition.x;
+        const dy = y - this.currentPosition.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        const transitionTime = distance / this.speed;
+        this._element.style.setProperty('--transitionTime', transitionTime + 's');
+    }
+
+    _setNewPosition(x, y) {
+        this._element.style.setProperty('--ballX', x + 'px');
+        this._element.style.setProperty('--ballY', y + 'px');
+    }
+
+    moveTo(x, y) {
+        this._setCurrentPosition(x, y);
+        this._setTransitionTime(x, y);
+        this._setNewPosition(x, y);
+    }
 }
